b764abbc9ca78adcdb8d3b5ca780432971f37bbb
chos9 <chos9@uw.edu>
2013-04-17 19:48:06 -0700

    homework2

--- homework2.py
+++ homework2.py
@@ -1,15 +1,15 @@
 #! /usr/bin/env python
 #
-#  Seongjin Cho (Josh)
-#  Student Number: 1130690
-#  Math 480A
+#	Seongjin Cho (Josh)
+#	Student Number: 1130690
+#	Math 480A
 #
-#  This is a program that determines wheter the given positive integer
-#  is Prime or not. It has two usable method: isPrime(n) and test().
-#  
+#	This is a program that determines wheter the given positive integer
+#	is Prime or not. It has two usable method: isPrime(n) and test().
+#
 
 from __future__ import division
-from math import sqrt, log, floor, ceil
+from math import sqrt, log, floor, ceil, pow
 from fractions import gcd
 
 def isPrime(n):
@@ -34,7 +34,6 @@ def isPrime(n):
 			else:
 				return result1
 	except:
-		print "Please try it again!"
 		raise
 	
 def step1(n):
@@ -51,12 +50,8 @@ def step1(n):
 		# Suppose $a,b$ are in $\N$ and $\sqrt[b]{a} \ne integer$.
 		# Then for all n in $\N$, $\sqrt[bn]{a} \ne integer$.
 		# So we only need to check 2,3,5,7,11,... <= int(log(n,3))
-		# I will make it better as I learn more about number theory
 		for i in b:
 			if  int(round(n**(1/i)))**i == n:
-				# To Do: I do not know how much time it takes to calculate
-				# n**(1/i). If it takes more than O(log^3(n)), I need
-				# to make my own better method
 				return "Composite"
 		return "Possibly Prime"
 
@@ -64,10 +59,6 @@ def step2(n):
 	"""Second Step: Find smallest r s.t. o_r(n) > log^2(n) where o_r(n) is
 	defined by min{k: n^k = 1 (mod r)}"""
 	for r in range(2, int(ceil(log(n, 2)**5)) + 1):
-		# To Do: r is bounded above by log^5(n) which needs proof,
-		# and I strongly feel that the log can have base 3 since
-		# we already elliminated odd integers. I will edit it if I
-		# prove it.
 		if gcd(r,n) == 1:
 			for k in range(1, int(log(n, 2)**2) + 1):
 				if n**k % r == 1:
@@ -78,12 +69,10 @@ def step2(n):
 def step3(n, r):
 	"""Third Step: if 1 < (a,n) < n for some a <= r, then return Composite
 	But if n <= r, return Prime. Otherwise, return Possibly Prime."""
-	for a in range(2,r):
+	for a in range(2,r+1):
 		if gcd(a,n) > 1 and gcd(a,n) < n:
 			return "Composite"
 	else:
-		# To Do: I do not really understand why this works. I need to
-		# study.. (Ahh too many stuffs to study!!)
 		if n <= r:
 			return "Prime"
 		else:
@@ -94,6 +83,7 @@ def step4(n, r):
 	whether 'a' satisfies the Fermet's little Theorem. If it satisfies for
 	all a, then it is definetely Prime!"""
 	# To Do: requires euler_phi function implementation in python
+	# this step is very heavy..... i need a method to optimize it
 	for a in range(1, int(floor(sqrt(euler_phi(r))*log(n,2))) + 1):
 		if a**n % n != a:
 			return "Composite"
@@ -103,10 +93,6 @@ def step4(n, r):
 def test():
 	"""Test whether my program works all right. Compares to the built in
 	sage method Primes(). WARNING: It takes lots of time"""
-	# To Do: I made this test method because I wasn't really sure whether
-	# my implementation will work flawlessly. I am now almost positive
-	# that I did not make any mistake. I now need to prove everything
-	# and fully understand what I just implemented.
 	print "Checking whether it finds all the front few primes."
 	count1 = 0
 	badcount = 0
